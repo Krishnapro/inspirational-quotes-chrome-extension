ff71281d668fdcec1f9fa2d264efc0b976a75aca
Krishna <krishnachaurasia1998@gmail.com>
2021-10-01 00:43:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    // DOM elements
+    const quote = document.querySelector("blockquote p");
+    const cite = document.querySelector("blockquote cite");
+  
+    async function updateQuote() {
+      // Fetch a random quote from the Quotable API
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      if (response.ok) {
+        // Update DOM elements
+        quote.textContent = data.content;
+        cite.textContent = data.author;
+      } else {
+        quote.textContent = "An error occured";
+        console.log(data);
+      }
+    }
+  
+    // call updateQuote once when page loads
+    updateQuote();
+  });
+  
